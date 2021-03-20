7cebc84822a0141092e5fc19e5fa80cac870512d
kronina <kroninanes@hotmail.com>
2021-03-20 16:10:43 +0100

    test commit

---
+++ resources/views/include/Master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+      @include('Partiel.Head')
+<body id="page-top">
+
+        @include('Partiel.Nav')
+
+@include('Partiel.Header')
+
+
+        @yield('content')
+        
+
+@include('Partiel.Footer')
+
+    </body>
+</html>
